617451176ce12c9b7b101caf9bb3b62fa28198a0
ForUKumar <forunaveenadds@gmail.com>
2025-07-26 14:35:59 +0530

    Create unzip_and_run.sh

---
+++ unzip_and_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download and unzip your backend ZIP from Bluehost
+ZIP_URL="https://palgunishomeocare.com/dailystickers/zipfiles/DailyStickersBackend.zip"
+
+curl -L -o backend.zip "$ZIP_URL"
+unzip backend.zip
+rm backend.zip
+
+npm install
